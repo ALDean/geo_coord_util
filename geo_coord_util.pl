b04e367fd8988e996a0b7729747294f7cc431361
ALDean <ALDean@users.noreply.github.com>
2014-07-22 17:15:23 -0500

    Add input validation to normalize_longitude Add additional test cases Add -v(erbose) option to unit test command line arguments

--- geo_coord_util.pl
+++ geo_coord_util.pl
@@ -1,10 +1,18 @@
+#!/usr/bin/perl
+
 package geo_coord_util;
+
 sub normalize_longitude($) {
 # normalize_longitude
 #   parameter(s): 
 #       longitude: in decimal degrees
 #   returns: longitude normalized to range +/- 180 degrees
 	my ($long) = @_;
+	if ( "$long" !~ /^[+-]*\d+\.?\d*$/ ) {
+		print STDERR "normalize_longitude: bad number input ($long) \n";
+		return(undef);
+
+	}
 	my $abs_long = abs($long);
 	while ( $abs_long > 180.0 ) {
 		$abs_long -= 360.0;
--- geo_coord_util_test.pl
+++ geo_coord_util_test.pl
@@ -1,4 +1,9 @@
+#!/usr/bin/perl
+
 require "geo_coord_util.pl";
+use Getopt::Std;
+
+getopts("v"); # Use -v to get verbose status of each test case
 
 sub normalize_longitude_test {
 # Unit Test for geo_coord_util:normalize_longitude( longitude ) ;
@@ -9,19 +14,25 @@ sub normalize_longitude_test {
 	$test_cases{-750} = -30;
 	$test_cases{-890} = -170;
 	$test_cases{920} = -160;
+	$test_cases{'920.0'} = -160;
+	$test_cases{'+920.0'} = -160;
+
 
 	$test_cases{85} = 85;
 	$test_cases{0} = 0;
 	$test_cases{-50.55} = -50.55;
 	$test_cases{-180} = -180.0;
 	$test_cases{180} = 180.0;
+	$test_cases{'badinput'} = undef;
 
 	my $passed = 1;
 	foreach $case ( keys(%test_cases) ) {
 		$normalized = &geo_coord_util::normalize_longitude($case);
 		if ( $test_cases{$case} != $normalized ) {
 			$passed = 0;	
-			print STDERR "failed: case $case result: $normalized expected: $test_cases{$case}\n";
+			print STDERR "failed: case $case \tresult: $normalized \texpected: $test_cases{$case}\n";
+		} elsif ( $opt_v ){
+			print STDERR "passed: case $case \tresult: $normalized \texpected: $test_cases{$case}\n";
 		}
 	}
 	return($passed);
